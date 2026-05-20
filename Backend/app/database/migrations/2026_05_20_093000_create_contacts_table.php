e84127d9adaeaaeace19a4fa172425d5f6f1b8b1
Dario Stacchini <dario36ds@gmail.com>
2026-05-20 12:05:25 +0200

    Aggiunte migrazioni

---
+++ Backend/app/database/migrations/2026_05_20_093000_create_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name");
+            $table->string("surname");
+            $table->text("description")->nullable();
+            $table->boolean("favourited")->default(false);
+            $table->foreignId("location_id")
+                ->constrained()
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
---
+++ Backend/app/database/migrations/2026_05_20_094704_create_phone_numbers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phone_numbers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer("phone_number");
+            $table->foreignId("contact_id")
+                ->constrained()
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('phone_numbers');
+    }
+};
