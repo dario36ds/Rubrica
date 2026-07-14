c4338786aa4b6af9b0bc55b72f837eed018ba7ac
dario36ds <dario36ds@gmail.com>
2026-07-14 23:44:58 +0200

    Aggiunta pagina index in frontend. Sistemati dati per far funzionare il progetto soltanto clonando il repo e accendendo container docker

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -11,4 +11,3 @@ Route::apiResource('contacts', ContactController::class);
 Route::apiResource('emails', EmailController::class);
 Route::apiResource('numbers', PhoneNumberController::class);
 Route::apiResource('locations', LocationController::class);
-
